fd0b00a26eacf0b2cd77f88ef3e971efb1026454
isthatsahil <sahil.verma997@gmail.com>
2021-10-19 23:30:28 +0530

    changes

--- app.js
+++ app.js
@@ -8,14 +8,15 @@ const bodyParser = require("body-parser");
 const LOGGER = require("morgan");
 const portfolioRoutes = require("./routes/portfolioRoutes");
 const imageUploader = require("./routes/imageUploaderRoutes");
-
+const { cloudinary } = require("./lib/cloudinary");
 const Images = require("./models/Images");
 
 const app = express();
 
 app.use(LOGGER("dev"));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(express.urlencoded({ limit: "50mb", extended: false }));
+app.use(express.json({ limit: "50mb" }));
+
 app.use(cors());
 
 //! Use of Multer
@@ -38,24 +39,47 @@ const storage = multer.diskStorage({
 const upload = multer({
   storage: storage,
 });
+app.get("/api/images", async (req, res) => {
+  const { resources } = await cloudinary.search
+    .expression("folder:certificates")
+    .sort_by("public_id", "desc")
+    .max_results(30)
+    .execute();
 
-app.use("/image/upload", upload.single("image"), (req, res) => {
-  if (!req.file) {
-    console.log("No file upload");
-  } else {
-    const imgUrl =
-      "https://cryptomonthly-server.herokuapp.com/upload/images/" +
-      req.file.filename;
-    const imageName = req.file.filename;
-    Images.create({
-      imageName: imageName,
-      imageUrl: imgUrl,
+  const publicIds = resources.map((file) => file.public_id);
+  res.send(publicIds);
+});
+app.post("/image/upload", async (req, res) => {
+  try {
+    const fileStr = req.body.data;
+    const uploadResponse = await cloudinary.uploader.upload(fileStr, {
+      upload_preset: "certificates",
+      use_filename: true,
     });
-    res.json({
-      msg: "Success",
-    });
+    res.json({ msg: "success" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ err: err });
   }
 });
+
+// app.use("/image/upload", upload.single("image"), (req, res) => {
+//   if (!req.file) {
+//     console.log("No file upload");
+//   } else {
+//     const imgUrl =
+//       "https://cryptomonthly-server.herokuapp.com/upload/images/" +
+//       req.file.filename;
+//     const imageName = req.file.filename;
+//     Images.create({
+//       imageName: imageName,
+//       imageUrl: imgUrl,
+//     });
+//     res.json({
+//       msg: "Success",
+//     });
+//   }
+// });
 
 app.use("/image/getAllImages", (req, res) => {
   Images.findAll()
@@ -80,7 +104,7 @@ app.use(portfolioRoutes);
 sequelize
   .sync()
   .then((result) => {
-    app.listen(process.env.PORT || 5000, () =>
+    app.listen(5000, () =>
       console.log("Server running on ::" + process.env.PORT || 5000)
     );
   })
